efa7b81aef21d0a9911caac277fce35412ffa9a2
Luke Barbuto <luke.barbuto@gmail.com>
2015-01-15 00:28:36 -0800

    Save files to the index

--- lib/launchpad/index.rb
+++ lib/launchpad/index.rb
@@ -1,12 +1,30 @@
 class Index
-  attr_accessor :target, :files
+  attr_accessor :files
 
-  def initialize(target = '.')
-    @target = Pathname.new target
+  def initialize(options = {})
+    @target_dir = Pathname.new options[:target_dir] || '.'
+    @index_location = options[:index_location] || 'index'
     @files = []
   end
 
-  def scan(target = @target)
+  def index_file
+    @index_file ||= File.open @index_location, 'w'
+  end
+
+  def save
+    scan if files.empty?
+
+    files.each do |data|
+      index_file.write "#{data[0]} - #{data[1]}\n"
+    end
+
+    index_file.close
+    @index_file = nil
+
+    self
+  end
+
+  def scan(target = @target_dir)
     target.each_child do |pathname|
       if pathname.file?
         @files << [ pathname, Digest::MD5.file(pathname) ]
--- spec/lib/launchpad/index_spec.rb
+++ spec/lib/launchpad/index_spec.rb
@@ -1,16 +1,14 @@
 require 'spec_helper'
 
 describe Index do
-  subject { described_class.new target }
+  subject { described_class.new args }
 
-  let(:target) { Pathname.new 'spec/fixtures/test_dir' }
+  let(:target_dir) { Pathname.new 'spec/fixtures/test_dir' }
+  let(:index_location) { target_dir.join 'index' }
 
-  describe '#scan' do
-    it 'loads the files in the root directory' do
-      expect{ subject.scan }
-        .to change { subject.files.count }
-        .from(0).to(10)
-    end
+  let(:args) do
+    { target_dir: target_dir,
+      index_location: target_dir.join('index') }
   end
 
   describe '#files' do
@@ -28,6 +26,55 @@ describe Index do
     it 'includes the md5 sums' do
       expect(subject.files.last.last.to_s)
         .to eq '5bbf5a52328e7439ae6e719dfe712200'
+    end
+  end
+
+  describe '#index_file' do
+    before do
+      allow(File).to receive(:open).with index_location, 'w'
+      subject.index_file
+    end
+
+    it 'opens a file at the index location' do
+      expect(File).to have_received(:open)
+        .with index_location, 'w'
+    end
+  end
+
+  describe '#save' do
+    let(:index_file) { StringIO.new }
+
+    before do
+      allow(subject)
+        .to receive(:index_file)
+        .and_return index_file
+
+      allow(index_file)
+        .to receive(:close)
+
+      subject.save
+      index_file.pos = 0
+    end
+
+    it 'saves all the data' do
+      expect(index_file.readlines.count).to eq 10
+    end
+
+    it 'saves in the correct format' do
+      expect(index_file.readlines.last)
+        .to match(/^[\w\/]*file_1.txt - 5b\w*$/)
+    end
+
+    it 'closes the file when done' do
+      expect(index_file).to have_received :close
+    end
+  end
+
+  describe '#scan' do
+    it 'loads the files in the root directory' do
+      expect{ subject.scan }
+        .to change { subject.files.count }
+        .from(0).to(10)
     end
   end
 end
