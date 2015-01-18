fe233a77eaa5822621b86399a1b3d15025cc2830
Luke Barbuto <luke.barbuto@gmail.com>
2015-01-17 18:56:16 -0800

    Add remote index loading and comparison

--- lib/launchpad/index.rb
+++ lib/launchpad/index.rb
@@ -1,56 +1,76 @@
+require 'open-uri'
+
 class Index
-  attr_accessor :local_files
+  attr_accessor :local, :remote
+  attr_writer :local_index_path, :remote_index_uri
 
   def initialize(options = {})
-    @target_dir = Pathname.new options[:target_dir] || '.'
-    @index_location = options[:index_location] || 'index'
-    @local_files = []
+    @target_dir = Pathname.new options[:target_dir]
+    @local_index_path = options[:local_index_path]
+    @remote_index_uri = options[:remote_index_uri]
   end
 
-  def local_index(mode: 'r')
-    @local_index ||= File.open @index_location, mode
+  def diff
+    (@remote - @local).map(&:first)
   end
 
-  def load
-    local_index.each_line do |line|
-      line.split(' | ').tap do |path, md5|
-        @local_files << [ Pathname.new(path), md5.chomp ]
-      end
-    end
-  rescue Errno::ENOENT
-    return false
-  else
-    close
-    true
+  def local
+    @local ||= parse_local
   end
 
-  def save
-    scan if local_files.empty?
-
-    local_files.each do |data|
-      local_index(mode: 'w').write "#{data[0]} | #{data[1]}\n"
-    end
-
-    close
-    self
+  def remote
+    @remote ||= parse_remote
   end
 
   def scan(target = @target_dir)
-    target.each_child do |pathname|
-      if pathname.file?
-        @local_files << [ pathname, Digest::MD5.file(pathname) ]
-      elsif pathname.directory?
-        scan pathname
-      end
-    end
-
-    self
+    @local = []
+    recursive_scan target
+    save
   end
 
   private
 
-  def close
-    local_index.close
-    @local_index = nil
+  def open_local
+    File.open(@local_index_path, 'w')
+  end
+
+  def parse(file)
+    file.read.split("\n").map do |line|
+      line.split(' | ').tap do |path, md5|
+        [ Pathname.new(path), md5.chomp ]
+      end
+    end
+  end
+
+  def parse_local
+    File.open(@local_index_path, 'r') { |file| parse file }
+  rescue Errno::ENOENT
+    false
+  end
+
+  def parse_remote
+    Kernel.open(@remote_index_uri) { |file| parse file }
+  rescue Errno::ENOENT
+    false
+  end
+
+  def recursive_scan(target)
+    target.each_child do |pathname|
+      if pathname.file?
+        @local << [ pathname, Digest::MD5.file(pathname) ]
+      elsif pathname.directory?
+        recursive_scan pathname
+      end
+    end
+  end
+
+  def save
+    file = open_local
+    local.each do |data|
+      file.write "#{data[0]} | #{data[1]}\n"
+    end
+
+    file.close
+    self
   end
 end
--- spec/lib/launchpad/index_spec.rb
+++ spec/lib/launchpad/index_spec.rb
@@ -4,123 +4,99 @@ describe Index do
   subject { described_class.new args }
 
   let(:target_dir) { Pathname.new 'spec/fixtures/test_dir' }
-  let(:index_location) { target_dir.join 'index' }
+  let(:local_index_path) { 'spec/fixtures/indexes/local' }
+  let(:remote_index_uri) { 'https://patch.cuemu.com/index' }
 
   let(:args) do
     { target_dir: target_dir,
-      index_location: index_location }
+      remote_index_uri: remote_index_uri,
+      local_index_path: local_index_path }
   end
 
-  describe '#local_files' do
-    before { subject.scan }
+  describe '#diff' do
+    let(:remote_index_uri) { 'spec/fixtures/indexes/remote' }
 
-    it 'returns an array of all file paths' do
-      expect(subject.local_files).to be_an Array
+    before do
+      subject.local
+      subject.local_index_path = remote_index_uri
+      subject.remote = subject.send :parse_local
     end
 
-    it 'includes the file paths' do
-      expect(subject.local_files.last.first.to_s)
-        .to eq 'spec/fixtures/test_dir/file_1.txt'
-    end
-
-    it 'includes the md5 sums' do
-      expect(subject.local_files.last.last.to_s)
-        .to eq '5bbf5a52328e7439ae6e719dfe712200'
+    it 'returns an array of needed files' do
+      expect(subject.diff).to eq [
+        'path/file_2.txt', 'path/file_4.txt', 'path/file_6.txt',
+        'path/file_7.txt', 'path/file_8.txt'
+      ]
     end
   end
 
-  describe '#local_index' do
-    before { allow(File).to receive(:open) }
+  describe '#local' do
+    context 'when an index is found' do
+      let(:local_index_path) { 'spec/fixtures/indexes/local' }
 
-    context 'with no arguments' do
-      before { subject.local_index }
-
-      it 'opens a file at the index location in read mode' do
-        expect(File).to have_received(:open)
-          .with index_location, 'r'
+      it 'returns an index of files' do
+        expect(subject.local.last.first).to eq 'path/file_5.txt'
+        expect(subject.local.last.last).to eq 'fake5md5'
       end
     end
 
-    context 'when passed a write mode' do
-      before { subject.local_index mode: 'w' }
-
-      it 'opens a file at the index location in write mode' do
-        expect(File).to have_received(:open)
-          .with index_location, 'w'
-      end
-    end
-  end
-
-  describe '#load' do
-    context 'when there is not an index file available' do
-      let(:index_location) { 'spec/fixtures/nope' }
+    context 'when an index is not found' do
+      let(:local_index_path) { 'spec/fixtures/nope' }
 
       it 'returns false' do
-        expect(subject.load).to be false
-      end
-    end
-
-    context 'when there is an index file available' do
-      let(:index_location) { 'spec/fixtures/index' }
-
-      it 'returns true' do
-        expect(subject.load).to be true
-      end
-
-      it 'loads a previously saved index to memory' do
-        expect{ subject.load }
-          .to change { subject.local_files.count }
-          .from(0).to(2)
-
-        expect(subject.local_files.last.first.to_s)
-          .to eq 'path/file_2.txt'
-
-        expect(subject.local_files.last.last.to_s)
-          .to eq 'fake2md5'
+        expect(subject.local).to be false
       end
     end
   end
 
-  describe '#save' do
+  describe '#remote' do
+    context 'when an index is found' do
+      before do
+        allow(Kernel).to receive :open
+        subject.remote
+      end
+
+      it 'opens and reads the remote file' do
+        expect(Kernel).to have_received(:open).with remote_index_uri
+      end
+    end
+
+    context 'when an index is not found' do
+      before { allow(Kernel).to receive(:open).and_raise Errno::ENOENT }
+
+      it 'returns false' do
+        expect(subject.remote).to be false
+      end
+    end
+  end
+
+  describe '#scan' do
     let(:local_index) { StringIO.new }
 
     before do
-      allow(subject)
-        .to receive(:local_index)
-        .and_return local_index
-
-      allow(local_index)
-        .to receive(:close)
-
-      subject.save
+      allow(subject).to receive(:open_local).and_return local_index
+      allow(local_index).to receive :close
+      subject.scan
       local_index.pos = 0
     end
 
-    it 'saves all the data' do
+    it 'updates with a full scan' do
+      expect(subject.local.size).to eq 10
+
+      expect(subject.local.last.first.to_s)
+        .to eq 'spec/fixtures/test_dir/file_1.txt'
+
+      expect(subject.local.last.last.to_s)
+        .to eq '5bbf5a52328e7439ae6e719dfe712200'
+    end
+
+    it 'caches all the data' do
       expect(local_index.readlines.count).to eq 10
     end
 
     it 'saves in the correct format' do
       expect(local_index.readlines.last)
         .to match(/^[\w\/]*file_1.txt \| 5b\w*$/)
-    end
-
-    it 'closes the file when done' do
-      expect(local_index).to have_received :close
-    end
-  end
-
-  describe '#scan' do
-    it 'loads the files in the root directory' do
-      expect{ subject.scan }
-        .to change { subject.local_files.count }
-        .from(0).to(10)
-
-      expect(subject.local_files.last.first.to_s)
-        .to eq 'spec/fixtures/test_dir/file_1.txt'
-
-      expect(subject.local_files.last.last.to_s)
-        .to eq '5bbf5a52328e7439ae6e719dfe712200'
     end
   end
 end
