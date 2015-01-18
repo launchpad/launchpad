183a32889a637edcefe7aacce2728f61e7976081
Luke Barbuto <luke.barbuto@gmail.com>
2015-01-18 10:42:04 -0800

    Add index documentation and minor tweaks

--- lib/launchpad/index.rb
+++ lib/launchpad/index.rb
@@ -1,29 +1,41 @@
 require 'open-uri'
 
+# Provides a diff between local and remote file data by maintaining a cache
+# with pathnames and md5 hex digests
 class Index
-  attr_reader :local, :remote
-
+  # @param [Hash] options
+  # @option options [String] :target_dir root instllation directory
+  # @option options [String] :local_index_path where the local index is cached
+  # @option options [String] :remote_index_uri uri for the remote index
   def initialize(options = {})
     @target_dir = Pathname.new options[:target_dir]
     @local_index_path = options[:local_index_path]
     @remote_index_uri = options[:remote_index_uri]
   end
 
+  # @return [Array<String>]
+  #   an array of files that are not present or do not match the remote
   def diff
     (remote - local).map(&:first)
   end
 
+  # @return [Array<Array<Pathname, String>>]
+  #   an array of pathnames and their md5 digests for the local install
   def local
     @local ||= parse_local
   end
 
+  # @return [Array<Array<Pathname, String>>]
+  #   an array of remote pathnames and their md5 digests from the patcher
   def remote
     @remote ||= parse_remote
   end
 
-  def scan(target = @target_dir)
+  # Updates {#local} with a full scan of the installation directory
+  # @return [self]
+  def scan
     @local = []
-    recursive_scan target
+    recursive_scan
     save
   end
 
@@ -53,7 +65,7 @@ class Index
     false
   end
 
-  def recursive_scan(target)
+  def recursive_scan(target = @target_dir)
     target.each_child do |pathname|
       if pathname.file?
         @local << [ pathname, Digest::MD5.file(pathname) ]
