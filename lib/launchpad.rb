f0ea33ed7c5020c0afb7668fb2f0c36cdaeb2488
Luke Barbuto <luke.barbuto@gmail.com>
2015-01-24 22:38:29 -0800

    Setup executable for launching the app

--- lib/launchpad.rb
+++ lib/launchpad.rb
@@ -1,6 +1,6 @@
-require 'launchpad/settings'
-require 'launchpad/index'
-require 'launchpad/gui/application'
+require 'lib/launchpad/settings'
+require 'lib/launchpad/index'
+require 'lib/launchpad/gui/application'
 
 # The main top level module for the launchpad gem.
 module Launchpad
