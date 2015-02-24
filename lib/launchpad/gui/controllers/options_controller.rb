820d78dea64eb0627baffd1b9f00bf71d4936ced
Luke Barbuto <luke.barbuto@gmail.com>
2015-02-23 21:11:42 -0800

    Add field value accessors to options

--- lib/launchpad/gui/controllers/options_controller.rb
+++ lib/launchpad/gui/controllers/options_controller.rb
@@ -4,9 +4,70 @@ module Launchpad
     include JRubyFX::Controller
     fxml 'options.fxml'
 
-    # Triggered when the cancel button is pressed
+    OPTIONS = [
+      :install,
+      :local_index_path,
+      :remote_index_uri,
+      :login_server,
+      :port
+    ]
+
+    OPTIONS.each do |option|
+      attr_reader option
+    end
+
+    def initialize
+      super
+      @stage.on_shown { display_values }
+    end
+
+    # Saves any changes to settings and closes the stage.
+    # Triggered when the cancel button is pressed.
+    def accept
+      save
+      @stage.close
+    end
+
+    # Aborts all changes and closes the stage.
+    # Triggered when the cancel button is pressed.
     def cancel
       @stage.close
+    end
+
+    # Loads all saved settings to the UI text fields.
+    def display_values
+      options.each do |option|
+        update_ui option, read(option)
+      end
+    end
+
+    private
+
+    def options
+      OPTIONS
+    end
+
+    def save
+      options.each do |option|
+        update option, read_ui(option)
+      end
+      Settings.save
+    end
+
+    def read_ui(option)
+      send(option).get_characters.to_s
+    end
+
+    def update_ui(option, value)
+      send(option).set_text value
+    end
+
+    def read(option)
+      Settings.read option
+    end
+
+    def update(option, value)
+      Settings.update option, value
     end
   end
 end
--- spec/lib/launchpad/gui/controllers/options_controller_spec.rb
+++ spec/lib/launchpad/gui/controllers/options_controller_spec.rb
@@ -9,11 +9,64 @@ describe Launchpad::OptionsController, type: :controller do
 
   let(:stage_double) { double 'stage', close: true }
 
+  let(:options) do
+    [:install, :local_index_path, :remote_index_uri, :login_server, :port]
+  end
+
+  let(:text_fields) do
+    options.reduce({}) do |options, option|
+      option_double = double(
+        option.to_s.capitalize,
+        get_characters: "#{option} value",
+        set_text: nil)
+      options.merge(option => option_double)
+    end
+  end
+
+  before do
+    described_class::OPTIONS.each do |option|
+      allow(subject).to receive(option).and_return text_fields[option]
+    end
+
+    allow(Launchpad::Settings).tap do |settings|
+      settings.to receive(:read).and_return 'value'
+      settings.to receive(:update).and_return true
+      settings.to receive(:save).and_return true
+    end
+  end
+
+  describe '#accept' do
+    before { subject.accept }
+
+    it 'closes the stage' do
+      expect(stage_double).to have_received :close
+    end
+
+    it 'updates all settings with values from the form' do
+      options.each do |option|
+        expect(Launchpad::Settings).to have_received(:update)
+          .with(option, "#{option} value")
+      end
+
+      expect(Launchpad::Settings).to have_received :save
+    end
+  end
+
   describe '#cancel' do
     before { subject.cancel }
 
     it 'closes the stage' do
       expect(stage_double).to have_received :close
+    end
+  end
+
+  describe '#display_values' do
+    before { subject.display_values }
+
+    it 'fills in the ui fields with current setting values' do
+      options.each do |option|
+        expect(text_fields[option]).to have_received(:set_text).with 'value'
+      end
     end
   end
 end
