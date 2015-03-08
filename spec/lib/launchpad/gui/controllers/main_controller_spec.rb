3a60563e4f48ce022ea052399b59e1f29a757217
Luke Barbuto <luke.barbuto@gmail.com>
2015-03-08 16:06:15 -0700

    Refactor main controller spec to allow extension
    Ensure main stage double is available in all specs
    Move options stage double to more specific scope

--- spec/lib/launchpad/gui/controllers/main_controller_spec.rb
+++ spec/lib/launchpad/gui/controllers/main_controller_spec.rb
@@ -1,46 +1,50 @@
 require 'spec_helper'
 
 describe Launchpad::MainController, type: :controller do
-  let(:stage_class) { described_class::Stage }
+  subject { described_class.new stage: stage_double }
+
   let(:stage_double) { double 'stage' }
 
-  let(:messages) do
-    [:title=, :always_on_top=, :resizable=, :fxml=, :show, :x=, :y=]
-  end
-
-  before do
-    allow(stage_class).to receive(:new).and_return stage_double
-    messages.each { |message| allow(stage_double).to receive message }
-
-    [:x, :y].each do |axis|
-      allow(Launchpad::Application)
-        .to receive_message_chain(:main_stage, axis)
-        .and_return 5
-    end
-  end
-
   describe '#show_options' do
-    before { subject.show_options }
+    let(:options_stage) { double 'stage' }
+    let(:stage_class) { described_class::Stage }
+
+    let(:messages) do
+      [:title=, :always_on_top=, :resizable=, :fxml=, :show, :x=, :y=]
+    end
+
+    before do
+      allow(stage_class).to receive(:new).and_return options_stage
+      messages.each { |message| allow(options_stage).to receive message }
+
+      [:x, :y].each do |axis|
+        allow(Launchpad::Application)
+          .to receive_message_chain(:main_stage, axis)
+          .and_return 5
+      end
+
+      subject.show_options
+    end
 
     it 'assigns a new options stage' do
-      expect(subject.instance_variable_get :@options).to be stage_double
+      expect(subject.instance_variable_get :@options).to be options_stage
     end
 
     it 'uses the options controller for the stage' do
-      expect(stage_double).to have_received(:fxml=)
+      expect(options_stage).to have_received(:fxml=)
         .with Launchpad::OptionsController
     end
 
     it 'sets the options stage attributes' do
-      expect(stage_double).to have_received(:title=).with 'Options'
-      expect(stage_double).to have_received(:always_on_top=).with true
-      expect(stage_double).to have_received(:resizable=).with false
-      expect(stage_double).to have_received(:x=).with 25
-      expect(stage_double).to have_received(:y=).with 45
+      expect(options_stage).to have_received(:title=).with 'Options'
+      expect(options_stage).to have_received(:always_on_top=).with true
+      expect(options_stage).to have_received(:resizable=).with false
+      expect(options_stage).to have_received(:x=).with 25
+      expect(options_stage).to have_received(:y=).with 45
     end
 
     it 'shows the options stage' do
-      expect(stage_double).to have_received :show
+      expect(options_stage).to have_received :show
     end
 
     context 'when called more than once' do
@@ -51,7 +55,7 @@ describe Launchpad::MainController, type: :controller do
       end
 
       it 'calls show every time' do
-        expect(stage_double).to have_received(:show).twice
+        expect(options_stage).to have_received(:show).twice
       end
     end
   end
